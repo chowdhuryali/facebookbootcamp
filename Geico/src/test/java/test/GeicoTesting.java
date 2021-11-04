d0151c44c66d217f7a185ac12cec9bf8bf16cec6
“ahmed” <chowad@gmail.com>
2021-11-04 09:25:37 -0700

    Ahmed_Geico_7testcases

--- Geico/src/test/java/test/GeicoTesting.java
+++ Geico/src/test/java/test/GeicoTesting.java
@@ -20,7 +20,7 @@ public class GeicoTesting {
         System.setProperty("webdriver.chrome.driver", "/Users/chowdhuryahmed/IdeaProjects/Web-Automation-Framework3/Generic/drivers/chromedriver");
 
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://www.geico.com/");
         driver.manage().window().maximize();
 
@@ -33,6 +33,19 @@ public class GeicoTesting {
         driver.findElement(By.xpath("//div[@class='zip-holder']")).click();
         Thread.sleep(3000);
 
+        driver.findElement(By.xpath("//input[@aria-label='5-Digit ZIP']")).sendKeys("92374");
+        Thread.sleep(3000);
+
+        driver.findElement(By.xpath("//*[@id=\"bundle_modal\"]/div/div[2]/form/div[3]/input")).click();
+        Thread.sleep(3000);
+
+        driver.findElement(By.xpath("/html/body/div[1]/div[2]/main/div/section/div/div/div/form/div[2]/div/input")).sendKeys("Chowdhury");
+        Thread.sleep(3000);
+
+        driver.findElement(By.xpath("/html/body/div[1]/div[2]/main/div/section/div/div/div/form/div[3]/div/input")).sendKeys("Ahmed");
+        Thread.sleep(3000);
+
+        driver.close();
     }
 
     @After
