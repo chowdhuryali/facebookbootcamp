c64cebe2339da30bd54aa60d87cde4d1f5e10155
“ahmed” <chowad@gmail.com>
2021-11-03 21:19:48 -0700

    Ahmed_Geico

--- Geico/src/test/java/test/GeicoTesting.java
+++ Geico/src/test/java/test/GeicoTesting.java
@@ -5,7 +5,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
@@ -15,27 +14,26 @@ public class GeicoTesting {
     public WebDriver driver;
 
     @Before
+
     @Test
     public void Upset() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "/Users/jawadsyed/IdeaProjects/JawadSeleniumProject/src/Drive/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/Users/chowdhuryahmed/IdeaProjects/Web-Automation-Framework3/Generic/drivers/chromedriver");
 
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
         driver.get("https://www.geico.com/");
         driver.manage().window().maximize();
 
+        driver.findElement(By.xpath("//div[@data-product='auto']")).click();
+        Thread.sleep(3000);
 
+        driver.findElement(By.xpath("//*[@id=\"bundleModalBtn\"]/p/a")).click();
+        Thread.sleep(3000);
 
-
-
-
-        driver.close();
-
+        driver.findElement(By.xpath("//div[@class='zip-holder']")).click();
+        Thread.sleep(3000);
 
     }
-
-
-
 
     @After
     public void after () {
