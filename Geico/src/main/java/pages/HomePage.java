833bb1470133d8d939b16601f7dd53c52d658eb2
ciahmed <chowad@gmail.com>
2021-11-20 08:59:33 -0800

    POM_25testcases_4pages_11testclasses

--- Geico/src/main/java/pages/HomePage.java
+++ Geico/src/main/java/pages/HomePage.java
@@ -1,8 +1,6 @@
 package pages;
 
 import base.CommonAPI;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
